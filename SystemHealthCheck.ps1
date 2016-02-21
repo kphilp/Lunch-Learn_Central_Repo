75b055817dd7bacc8a90becf02a540769d21fd82
Kevin <kevinphilp@hotmail.co.uk>
2016-02-21 14:36:17 +0000

    First Draft of System Health Check PS Script
    To be reviewed by Chris.

---
+++ SystemHealthCheck.ps1
@@ -0,0 +1,11 @@
+﻿# Retrieve the current memory utilization for the Localhost
+Get-Counter '\Memory\Available Mbytes'
+
+#Retrieve the current CPU utilization for the Localhost
+Get-Counter '\processor(_Total)\% Processor Time'
+
+# Retrieve a list of all drives for the Localhost
+Get-WmiObject Win32_LogicalDisk -ComputerName Localhost -Filter "DriveType = 3"
+
+# Retrieve IP Address for the Localhost
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter 'IPEnabled = True'
